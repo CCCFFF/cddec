ddcad28d7c3ab68d38abe2e2e58e7eefdfa36db0
Craig <craig.frey@hotmail.com>
2013-09-02 17:27:42 -0500

    set up model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
+has_many :homes
+has_many :rooms, :through => :homes
+has_many :pictures, :through => :rooms
 
 validates :email, :presence => true, :uniqueness => true
 end
